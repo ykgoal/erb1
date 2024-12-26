3ed34319820c5a6dc69874b4b56445d040ae3f5f
ykgoal <ykgoal@gmail.com>
2024-12-26 14:04:01 +0800

    step 3 github init

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Msglist
 
 # Register your models here.
+
+class MsglistAdmin(admin.ModelAdmin):
+    list_display = 'id', 'name',
+    list_display_links = 'id', 'name',
+    search_fields = 'id', 'name',
+    list_filter = 'name',
+    list_per_page = 25
+
+
+admin.site.register(Msglist, MsglistAdmin)
--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Msglist
 
 # Create your views here.
 
 
 def index(request):
 
-    context = {}
+    msglist = Msglist.objects.all()
+    msglist = msglist[0]
+
+    context = {
+        'msglist' : msglist,
+    }
 
     return render(request, 'pages/index.html', context)
